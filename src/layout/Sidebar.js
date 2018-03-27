f96fb91617b4ffc182d6203621198e376c072ba5
jeff <jeff@jeffHome.lan>
2018-03-27 13:51:10 +0200

    searchCategories component  refacto

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -29,7 +29,7 @@ class Sidebar extends Component {
         const items = this.state.itemsInCategory;
         const itemsList = items.map((item, index) => 
             
-            <div className="form-check">
+            <div className="form-check" key={index}>
                 <input className="form-check-input" type="checkbox" value="" id="programming" key={index}/>
                 <label className="form-check-label text-muted small text-capitalize pl-2" htmlFor="programming">
                     {item}
@@ -41,9 +41,10 @@ class Sidebar extends Component {
 
         const categories = this.state.categoriesList;
         const categoriesItems = Object.values(categories).map((category, index) =>
-            <div>
+            <div key={index}>
                 <div className="text-gray text-capitalize small font-weight-light pb-2" key={index}>{category}</div>
                 {itemsList}
+                <hr />
             </div>
         );
 
@@ -53,37 +54,7 @@ class Sidebar extends Component {
         return (
             <div className="px-5">
                 <hr />
-                {categoriesItems}
-                <div className="text-gray text-capitalize small font-weight-light pb-2">programming</div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="programming" />
-                    <label className="form-check-label text-muted small text-capitalize pl-2" htmlFor="programming">
-                        javascript
-                    </label>
-                    <span className="float-right small pt-1 text-info">15</span>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="reactjs" />
-                    <label className="form-check-label text-muted small text-capitalize pl-2" htmlFor="reactjs">
-                        reactjs
-                    </label>
-                    <span className="float-right small pt-1 text-info">8</span>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="angular" />
-                    <label className="form-check-label text-muted small text-capitalize pl-2" htmlFor="angular">
-                        angular
-                    </label>
-                    <span className="float-right small pt-1 text-info">19</span>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="fullstack-js" />
-                    <label className="form-check-label text-muted small text-capitalize pl-2" htmlFor="fullstack-js">
-                        fullstack js
-                    </label>
-                    <span className="float-right small pt-1 text-info">5</span>
-                </div>
-                <hr />
+                {categoriesItems}                
             </div>
         )
     }
--- src/layout/searchCoach.js
+++ src/layout/searchCoach.js
@@ -1,36 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import * as firebase from 'firebase';
 import '../firebase';
-
 
 class SearchCoach extends Component {
 
     constructor(props) {
-
         super(props);
         this.state = {
             categoriesList: {},
+            value: 'programming',
+            dropdownTitle:'Search by category',
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        
     }
 
     componentDidMount() {
 
-        this.firebaseRef = firebase.database().ref().child('categories');
-        this.firebaseCallback = this.firebaseRef.on('value', snap => {
-            this.setState({ categoriesList: Object.keys(snap.val()) });
-        });
+        this.firebaseRef = firebase
+            .database()
+            .ref()
+            .child('categories');
+        this.firebaseCallback = this
+            .firebaseRef
+            .on('value', snap => {
+                this.setState({
+                    categoriesList: Object.keys(snap.val())
+                });
+            });
     }
 
-    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        event.preventDefault();
+
+    }
+
+    handleSubmit(event) {
+        this.setState({value: event.target.value});
+        event.preventDefault();
+        this.setState({value: ''});
+    }
+
+    handleClick(event) {
+        this.setState({value: event.target.text});
+        event.preventDefault();
+    }
+
+   
+
     render() {
         const categories = this.state.categoriesList;
-        const todoItems = Object.values(categories).map((category, index) =>
-            <a className="dropdown-item" href="" key={index}>{category}</a>
-        );
-        
+        const todoItems = Object
+            .values(categories)
+            .map((category, index) => <a className="dropdown-item" href=""  key={index}>{category}
+            </a>);
+
         return (
 
-            <form className="form-inline my-2 my-lg-0 ">
+            <form className="form-inline my-2 my-lg-0 " onSubmit={this.handleSubmit}>
                 <div className="input-group">
                     <div className="input-group-prepend">
                         <button
@@ -38,16 +69,19 @@ class SearchCoach extends Component {
                             type="button"
                             data-toggle="dropdown"
                             aria-haspopup="true"
-                            aria-expanded="false"
-                        >Search your coach</button>
-                        <div className="dropdown-menu">
+                            aria-expanded="false">{this.state.dropdownTitle}</button>
+                        <div className="dropdown-menu" onClick={this.handleClick}>
                             {todoItems}
                         </div>
                     </div>
                     <input
                         type="text"
                         className="form-control"
-                        aria-label="Text input with dropdown button"/>
+                        aria-label="Text input with dropdown button"
+                        placeholder={this.state.value}
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        />
                     <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Go !</button>
                 </div>
             </form>
